de296eb3d5f5f33f8dc10d8f222daa7851c9d1d0
SarahMulloy <sarah.m.mulloy@gmail.com>
2026-03-05 21:37:18 -0600

    Update through question 5

--- data/prescriber_exercise_SM.sql
+++ data/prescriber_exercise_SM.sql
@@ -1,19 +1,23 @@
 -- Prescribers Database
 -- For this exericse, you'll be working with a database derived from the Medicare Part D Prescriber Public Use File. More information about the data is contained in the Methodology PDF file. See also the included entity-relationship diagram.
 
-
-
-
 -- 1a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
 
 -- in the 'prescription' table, look at 'npi' and 'total_calim_count'
 -- group by npi, since each row is a event
 
-SELECT npi, COUNT(drug_name) as total_drug, SUM(total_claim_count) as total_claim
-FROM public.prescription
-GROUP BY npi
-ORDER BY SUM(total_claim_count) DESC
-LIMIT 5;
+SELECT
+	NPI,
+	COUNT(DRUG_NAME) AS TOTAL_DRUG,
+	SUM(TOTAL_CLAIM_COUNT) AS TOTAL_CLAIM
+FROM
+	PUBLIC.PRESCRIPTION
+GROUP BY
+	NPI
+ORDER BY
+	SUM(TOTAL_CLAIM_COUNT) DESC
+LIMIT
+	5;
 
 --**answer: the higherst prescriber is npi #1881634483 with 99707 total claims
 
@@ -67,7 +71,7 @@ FROM
 	PRESCRIPTION AS PS
 	LEFT JOIN PRESCRIBER AS PR ON PS.NPI = PR.NPI
 	LEFT JOIN DRUG AS DR ON PS.DRUG_NAME = DR.DRUG_NAME
---WHERE DR.OPIOID_DRUG_FLAG = Y ## no idea why this isnt working 
+WHERE DR.OPIOID_DRUG_FLAG LIKE 'Y' --## no idea why this isnt working 
 GROUP BY
 	PR.SPECIALTY_DESCRIPTION,
 	DR.OPIOID_DRUG_FLAG
@@ -77,12 +81,87 @@ ORDER BY
 
 
 -- 2c. Challenge Question: Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
+-- prescriber table has 'specialty_description' -> get list
+-- prescription table --> merge the prescriber table on the npi column
+-- then use EXCEPT to pull out the specialties in the first tbale NOT the second table 
+
+(SELECT specialty_description
+FROM prescriber)
+EXCEPT
+(SELECT specialty_description
+FROM prescription as pr
+INNER JOIN prescriber as pb on pb.npi = pr.npi);
+
+--**answer: there are 15 specialties that have no prescriptions
+
 -- 2d. Difficult Bonus: Do not attempt until you have solved all other problems! For each specialty, report the percentage of total claims by that specialty which are for opioids. Which specialties have a high percentage of opioids?
 -- 3a. Which drug (generic_name) had the highest total drug cost?
--- 3b. Which drug (generic_name) has the hightest total cost per day? Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.
--- 4a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs. Hint: You may want to use a CASE expression for this. See https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-case/
--- 4b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
--- 5a. How many CBSAs are in Tennessee? Warning: The cbsa table contains information for all states, not just Tennessee.
+
+SELECT MAX(total_drug_cost) as max_cost, generic_name 
+FROM drug as d
+INNER JOIN prescription as pr on pr.drug_name = d.drug_name 
+GROUP BY generic_name
+ORDER BY MAX(total_drug_cost) DESC;
+
+--**answer: PIRFENIDONE had the highest total drug cost of 2829174.3
+
+
+-- 3b. Which drug (generic_name) has the hightest total cost per day? 
+--Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.
+SELECT round(MAX((total_drug_cost / total_day_supply)), 2) as cost_per_day, generic_name 
+FROM drug as d
+INNER JOIN prescription as pr on pr.drug_name = d.drug_name 
+GROUP BY generic_name
+ORDER BY cost_per_day DESC;
+
+--**ANSWER: Immun glob had highest cost_per_day at 7141.11 
+
+
+-- 4a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs. 
+--Hint: You may want to use a CASE expression for this. See https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-case/
+
+SELECT
+	GENERIC_NAME,
+	CASE
+		WHEN OPIOID_DRUG_FLAG = 'Y' THEN 'opioid'
+		WHEN ANTIBIOTIC_DRUG_FLAG = 'Y' THEN 'antibiotic'
+		WHEN OPIOID_DRUG_FLAG = 'N'
+		AND ANTIBIOTIC_DRUG_FLAG = 'N' THEN 'neither'
+	END as 'drug_type'
+FROM
+	DRUG;
+
+
+-- 4b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. 
+-- Hint: Format the total costs as MONEY for easier comparision.
+SELECT
+	SUM(total_drug_cost) as all_total_drug_cost
+	,CASE
+		WHEN OPIOID_DRUG_FLAG = 'Y' THEN 'opioid'
+		WHEN ANTIBIOTIC_DRUG_FLAG = 'Y' THEN 'antibiotic'
+		WHEN OPIOID_DRUG_FLAG = 'N'
+		AND ANTIBIOTIC_DRUG_FLAG = 'N' THEN 'neither'
+	END as drug_type
+FROM
+	DRUG as d
+INNER JOIN prescription as pr on d.drug_name = pr.drug_name
+GROUP BY drug_type 
+ORDER BY all_total_drug_cost DESC;
+
+--**answer: more money was spent on opioids with over 100 million compared to 38 million for antibiotics
+
+-- 5a. How many CBSAs are in Tennessee? 
+-- Warning: The cbsa table contains information for all states, not just Tennessee.
+-- have to merge fips_county with csba table to get the state info 
+
+SELECT COUNT(cbsa)
+FROM cbsa as c 
+INNER JOIN fips_county as f on c.fipscounty = f. fipscounty
+WHERE state = 'TN' ;
+
+--** answer: there are 42 CBSA in tennessee
+
+
 -- 5b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
 -- 5c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
 -- 6a. Find all rows in the prescription table where total_claims is at least 3000. Report the drug_name and the total_claim_count.
